4919e7e802f0a43daec0dce4b7c88dd78861a3cf
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-26 23:37:38 +0100

    start design

---
+++ Desktop/developer_akademie  /ringoffire/ring-of-fire/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+
+
+const routes: Routes = [
+  {path: '', component: StartscreenComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Desktop/developer_akademie  /ringoffire/ring-of-fire/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartscreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
