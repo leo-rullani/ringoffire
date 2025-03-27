fd992f980f4f19bf4f691ee5f1285195c0ff728f
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-27 21:24:25 +0100

    show the played cards

--- Desktop/developer_akademie  /ringoffire/ring-of-fire/src/app/game/game.component.ts
+++ Desktop/developer_akademie  /ringoffire/ring-of-fire/src/app/game/game.component.ts
@@ -27,12 +27,14 @@ export class GameComponent {
   takeCard() {
         if(!this.pickCardAnimation) {
         this.currentCard = this.game.stack.pop();
-        console.log(this.currentCard);
         this.pickCardAnimation = true;
+        console.log('New Card: ' + this.currentCard);
+        console.log('Game is', this.game);
 
         setTimeout(()=> {
+          this.game.playedCards.push(this.currentCard);
           this.pickCardAnimation = false;
-      }, 1500);
+      }, 1000);
     }
   }
 }
