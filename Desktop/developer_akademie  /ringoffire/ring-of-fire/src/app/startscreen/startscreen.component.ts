b700eebc21e7f8be042377fd5e1bbbf339507893
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-27 10:05:36 +0100

    start game

--- Desktop/developer_akademie  /ringoffire/ring-of-fire/src/app/startscreen/startscreen.component.ts
+++ Desktop/developer_akademie  /ringoffire/ring-of-fire/src/app/startscreen/startscreen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./startscreen.component.scss']
 })
 export class StartscreenComponent {
+
+constructor(private router: Router) {}
+
+  newGame() {
+    // Start game
+    this.router.navigateByUrl('/game');
+  }
 
 }
