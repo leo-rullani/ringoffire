70e53ea303c51fa7b59cb55d557dff8ce691f82b
Leugzim Rullani <leugzimrullani@outlook.com>
2025-06-02 14:33:44 +0200

    mixin generics

--- Desktop/developer_akademie/django_drf/market_app/api/urls.py
+++ Desktop/developer_akademie/django_drf/market_app/api/urls.py
@@ -1,16 +1,18 @@
+# market_app/api/urls.py
+
 from django.urls import path
-from .views import markets_view, market_single_view, sellers_view, sellers_single_view
+from .views import MarketsView, MarketDetailView, sellers_view, sellers_single_view
 
 urlpatterns = [
     # List- und Create-Endpunkt für Markets
-    path('market/', markets_view, name='market_list'),
+    path('market/', MarketsView.as_view(), name='market_list'),
 
-    # Detail-Endpunkt für einen einzelnen Market (mit Slash und Name)
-    path('market/<int:pk>/', market_single_view, name='market-detail'),
+    # Mixin-basierte Detail-View für einen einzelnen Market
+    path('market/<int:pk>/', MarketDetailView.as_view(), name='market-detail'),
 
     # List- und Create-Endpunkt für Sellers
     path('seller/', sellers_view, name='seller_list'),
 
-    # Detail-Endpunkt für einen einzelnen Seller (mit Slash und Name)
+    # Detail-Endpunkt für einen einzelnen Seller
     path('seller/<int:pk>/', sellers_single_view, name='seller_single'),
 ]
--- Desktop/developer_akademie/django_drf/market_app/api/views.py
+++ Desktop/developer_akademie/django_drf/market_app/api/views.py
@@ -1,25 +1,73 @@
+# market_app/api/views.py
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MarketSerializer, SellerSerializer, MarketHyperlinkedSerializer
 from market_app.models import Market, Seller
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+
+
+class MarketsView(generics.ListCreateAPIView):
+    
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+
+    # def get(self, request, *args, **kwargs):
+    #   return self.list(request, *args, **kwargs)
+    
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
+
+
+class MarketDetailView(mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.DestroyModelMixin,
+                       generics.GenericAPIView):
+    """
+    Detail-View für ein einzelnes Market-Objekt:
+    - GET    -> RetrieveModelMixin
+    - PUT    -> UpdateModelMixin
+    - DELETE -> DestroyModelMixin
+    """
+
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
 
 @api_view(['GET', 'POST'])
 def markets_view(request):
 
     if request.method == 'GET':
         markets = Market.objects.all()
-        serializer = MarketHyperlinkedSerializer(markets, many=True, context={'request': request}, fields=('id', 'name', 'net_worth'))
+        serializer = MarketHyperlinkedSerializer(
+            markets,
+            many=True,
+            context={'request': request},
+            fields=('id', 'name', 'net_worth')
+        )
         return Response(serializer.data)
 
     if request.method == 'POST':
         serializer = MarketSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
 @api_view(['GET', 'DELETE', 'PUT'])
 def market_single_view(request, pk):
 
@@ -44,6 +92,7 @@ def market_single_view(request, pk):
         market.delete()
         return Response(serializer.data)
 
+
 @api_view()
 def sellers_single_view(request, pk):
     if request.method == 'GET':
@@ -51,6 +100,7 @@ def sellers_single_view(request, pk):
         serializer = SellerSerializer(seller, context={'request': request})
         return Response(serializer.data)
     
+
 @api_view(['GET', 'POST'])
 def sellers_view(request):
 
